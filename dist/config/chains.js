464cb021c2e1944db67e5b047cbd99da51394347
kenilshahh <kenilshah1505@gmail.com>
2025-06-11 19:16:48 +0530

    added supersim

--- dist/config/chains.js
+++ dist/config/chains.js
@@ -1,8 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.defaultChains = void 0;
-const interopAlpha_1 = require("@eth-optimism/viem/src/chains/interopAlpha");
 exports.defaultChains = [
-    interopAlpha_1.interopAlpha0, interopAlpha_1.interopAlpha1
+    exports.supersimL2A, exports.supersimL2B
 ];
 //# sourceMappingURL=chains.js.map
